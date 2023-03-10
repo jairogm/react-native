1f172e7ea9445536376bfa0f3ca69323a389ecf0
503 <503@academicos.cesde.edu.co>
2023-03-09 19:20:16 -0500

    hey

--- calculadoraBasica/App.js
+++ calculadoraBasica/App.js
@@ -1,31 +1,30 @@
 import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import Banner from './components/Banner';
-
 export default function App() {
-  //DEFINIR LAS VARIABLES DE ESTADO DEL COMPONENTE....
-  const[valor1,setValor] = useState('');
-  const[valor2,setValor2] = useState('');
-  const[resultado,setResultado] = useState('');
+  // Definir las variables de estado del componente
+  const [valor1, setValor1] = useState('');
+  const [valor2, setValor2] = useState('');
+  const [resultado, setResultado] = useState('');
   return (
     <View style={styles.container}>
-      <View style={[styles.container,{flex:1,backgroundColor:'white'}]}>
-        <Banner name="Calculadora2"/>
+      <View style={[styles.container,{flex:1,backgroundColor:'orange'}]}>
+        <Banner name="calculadora"></Banner>
       </View>
-      <View style={[styles.container,{flex:5, backgroundColor:'yellow'}]}>
-        <Text style={{fontWeight:'bold'}}>Calculadora de BASIC</Text>
+      <View style={[styles.container,{flex:5,backgroundColor:'powderblue'}]}>
+        <Text style={{fontWeight:'bold'}}>Calculadora Básica</Text>
         <Text>Valor 1</Text>
         <TextInput
-          placeholder='Ingrese el valor1'
+          placeholder='Ingrese valor 1'
           style={styles.inputs}
-          onChange={valor1 => setValor(valor1)}
+          onChangeText={valor1 => setValor1(valor1)}
           value={valor1}
         />
         <Text>Valor 2</Text>
         <TextInput
-          placeholder='Ingrese el valor2'
+          placeholder='Ingrese valor 2'
           style={styles.inputs}
-          onChange={valor2 => setValor2(valor2)}
+          onChangeText={valor2 => setValor2(valor2)}
           value={valor2}
         />
         <Text>Resultado</Text>
@@ -33,19 +32,14 @@ export default function App() {
           style={styles.inputs}
           value={resultado}
         />
-      </View> 
-      <View style={[styles.container,{flex:1, backgroundColor:'gray'}]}>
+
+      </View>
+      <View style={[styles.container,{flex:1,backgroundColor:'gray'}]}>
         <Text>Pie</Text>
       </View>
     </View>
   );
 }
-
-// function Banner(props){
-//   return(
-//     <Image source={require(`./assets/images/${props.name}.png`)} style={{width:'100%',height:'100%',resizeMode:'stretch'}}></Image>
-//   )
-// }
 
 const styles = StyleSheet.create({
   container: {
@@ -53,12 +47,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     width:'100%'
   },
-  inputs: {
-    color:'orange', 
-    borderRadius:5, 
-    padding:10, 
-    borderWidth:2, 
-    borderColor:'blue', 
+  inputs:{
+    color:'blue',
+    borderRadius:5,
+    padding:10,
+    borderWidth:2,
+    borderColor:'green',
     textAlign:'center'
   }
+  
 });
--- calculadoraBasica/components/Banner.jsx
+++ calculadoraBasica/components/Banner.jsx
@@ -1,8 +1,7 @@
 import { Image, Text } from "react-native";
 
 export default function Banner(props){
-  return(
-
-    <Image source={require(`../assets/images/${props.name}.png`)} style={{width:'100%',height:'100%',resizeMode:'stretch'}}></Image>
-  )
-}   
+    return(
+      <Image source={require(`../assets/images/${props.name}.png`)} style={{width:'100%',height:'100%',resizeMode:'stretch'}}></Image>
+    );
+}
